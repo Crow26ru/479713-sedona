90b8e8c6d4e2c1d5d6f5dabc66fab283c65eab9d
Семен <7tacson7@gmail.com>
2018-09-04 13:19:36 +0300

    Улучшено поведение анимации формы

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 var formOrder = document.querySelector(".map form");
 var buttonListeningForm = document.querySelector(".btn-open-form");
-formOrder.classList.add("form-close");
+formOrder.classList.add("start-status");
 
 
 buttonListeningForm.addEventListener("click", function(e) {
     e.preventDefault();
-    if (formOrder.classList.contains("form-close")) {
+    formOrder.classList.remove("start-status");
+    if (!formOrder.classList.contains("form-close") && !formOrder.classList.contains("form-open")) {
+        formOrder.classList.add("form-open");
+    } else if (formOrder.classList.contains("form-close")) {
+        var focusOnInput = formOrder.querySelector("input");
+        
         formOrder.classList.remove("form-close");
         formOrder.classList.add("form-open");
+        focusOnInput.focus();
     } else {
         formOrder.classList.remove("form-open");
         formOrder.classList.add("form-close");
